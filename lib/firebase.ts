e6e776037fc39551aed38d7917ec8dbe008d789a
princetnwr723-cloud <princetnwr723@gmail.com>
2026-09-12 06:36:17 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,7 +10,6 @@ import {
   type Auth,
 } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
-import { getStorage, type FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -25,7 +24,6 @@ const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseCon
 
 export const auth: Auth = getAuth(app);
 export const db: Firestore = getFirestore(app);
-export const storage: FirebaseStorage = getStorage(app);
 export const googleProvider = new GoogleAuthProvider();
 
 export default app;
